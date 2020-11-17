f8cbddcd2a618b01e90e2150271ad561f1a12fed
hosam <hosamelmalt@gmail.com>
2020-11-17 15:47:55 +0200

    Initial Commit

---
+++ Music Apple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music Apple
+//
+//  Created by hosam on 11/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music Apple/Music_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AppleApp.swift
+//  Music Apple
+//
+//  Created by hosam on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Music_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
